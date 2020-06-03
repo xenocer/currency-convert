0803ee8faca2e412b99e4b91f28177d92724db08
Xenos <xenocer>
2020-06-04 03:34:11 +0700

    test body webhook

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,6 +1,17 @@
 const Router = require('koa-router');
+const line = require('@line/bot-sdk');
+const _ = require('lodash')
 const router = new Router();
-
+const config = require('config');
+const configData = {
+    channelAccessToken:  config.get('LINE_CHANNEL_ACCESS_TOKEN'),
+    channelSecret: config.get('LINE_CHANNEL_SECRET'),
+};
+const client = new line.Client(configData);
+const message = {
+    type: 'text',
+    text: 'Hello World!'
+};
 router.get('/', async (ctx) => {
     ctx.body = {
         message: 'Currency Convert API'
@@ -8,7 +19,9 @@ router.get('/', async (ctx) => {
 })
 router.post('/webhook', async (ctx) => {
     ctx.status = 200
-    console.log(ctx.request.body)
+    let body = _.get(ctx.request.body, 'events', {})
+    let content = body[0]
+    console.log(content)
 })
 
 module.exports = router;
