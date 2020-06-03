739a00814db5793aa6d21d8b5158c46c50f57124
Xenos <xenocer>
2020-06-04 02:37:07 +0700

    log body

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,9 @@
 const Koa = require('koa');
 const indexRoutes = require('./routes/index');
-
+const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const PORT = 3001;
-
+app.use(bodyParser());
 app.use(indexRoutes.routes());
 
 const server = app.listen(PORT, () => {
